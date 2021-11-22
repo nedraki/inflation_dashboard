4e123d162e01722d83a2ae291bbfdc68a37b9398
BeCode <dominguezalbermar@gmail.com>
2021-11-22 13:02:20 +0100

    Formatting with black

--- utils/data_reader.py
+++ utils/data_reader.py
@@ -12,9 +12,11 @@ country-code: Relation currency & countries
 """
 
 # Defining path to files:
-path_to_big_mac = os.path.abspath('./data/big_mac_index/big-mac-full-index.csv')
+path_to_big_mac = os.path.abspath("./data/big_mac_index/big-mac-full-index.csv")
 path_to_currencies = "sqlite:///data/CurrenciesDataBase_V2.db"
-path_to_country = os.path.abspath("./data/country-currency_code/country-code-to-currency-code-mapping.csv")
+path_to_country = os.path.abspath(
+    "./data/country-currency_code/country-code-to-currency-code-mapping.csv"
+)
 
 # Reading files:
 
@@ -22,11 +24,11 @@ df_big_mac = pd.read_csv(path_to_big_mac)
 df_big_mac["date"] = pd.to_datetime(df_big_mac.date)
 df_big_mac = df_big_mac.sort_values(by="date", axis=0)
 
-df_market = pd.read_sql('currencies_vs_btc', path_to_currencies)
+df_market = pd.read_sql("currencies_vs_btc", path_to_currencies)
 df_country = pd.read_csv(path_to_country)
 
-## Merge market data with countries dataset 
-df_market_mapping = pd.merge(df_market, df_country, on = 'currency_code', how = 'inner')
+## Merge market data with countries dataset
+df_market_mapping = pd.merge(df_market, df_country, on="currency_code", how="inner")
 df_market_mapping.dropna(inplace=True)
 
 ###FILTERING DIRTY DATA POINTS FOR VENEZUELA
@@ -38,12 +40,12 @@ df_filtered = df_market_mapping[df_market_mapping.country == "Venezuela"]
 # df_filtered["implicit_exchange"] = df_filtered['avg_1h'].divide(df_filtered["avg_24h_usd"], fill_value = None)
 df_filtered = df_filtered[df_filtered.implicit_exchange > 100]
 index_to_drop = df_filtered.index.tolist()
-df_filtered_final = df_market_mapping.drop(index = index_to_drop, inplace=True)
+df_filtered_final = df_market_mapping.drop(index=index_to_drop, inplace=True)
 
 ##############################
 
-class DataReader :
 
+class DataReader:
     def __init__(self):
         "Data Reader"
 
@@ -52,14 +54,16 @@ class DataReader :
     def pct_change(self, df, currency_code, date_reference):
 
         """### Pct performs calculation of percentual variation on exhange rate,
-            date_reference = "2021-10-03 First data point on dataset"""
-        
+        date_reference = "2021-10-03 First data point on dataset"""
+
         ## Select currency for calculations:
-        df_exchange = df[df['currency_code'] == currency_code ]
+        df_exchange = df[df["currency_code"] == currency_code]
 
         ## Select date to calculate reference
 
-        df_date_reference = df_exchange.loc[(df_exchange.date).dt.date == pd.Timestamp(date_reference)]
+        df_date_reference = df_exchange.loc[
+            (df_exchange.date).dt.date == pd.Timestamp(date_reference)
+        ]
 
         ### Averare exchange rate for day of reference:
 
@@ -67,31 +71,35 @@ class DataReader :
 
         ### Percentual Variation
 
-    #     equation = ((df_exchange.implicit_exchange / implicit_exchange_reference)-1)*100
-        
+        #     equation = ((df_exchange.implicit_exchange / implicit_exchange_reference)-1)*100
+
         return implicit_exchange_reference
 
     def metrics(self, df, currency_code):
 
-        implicit_exchange = df[df['currency_code'] == currency_code ]
-        
-        metric_exchange_rate = round(implicit_exchange["implicit_exchange"].values[-1], 2 )
+        implicit_exchange = df[df["currency_code"] == currency_code]
+
+        metric_exchange_rate = round(
+            implicit_exchange["implicit_exchange"].values[-1], 2
+        )
 
         metric_pct = round(implicit_exchange["pct"].values[-1], 2)
 
-        metric_volume_btc = round(implicit_exchange['volume_btc'].values[-1], 4)
+        metric_volume_btc = round(implicit_exchange["volume_btc"].values[-1], 4)
 
         last_update = implicit_exchange["date"].values[-1]
         # Convert full timestampt to Day format
-        last_update = np.datetime_as_string(last_update, unit = "D")
+        last_update = np.datetime_as_string(last_update, unit="D")
 
         return metric_exchange_rate, metric_pct, metric_volume_btc, last_update
 
-    def top_variation_value(self, number, columns, df = df_market_mapping):
+    def top_variation_value(self, number, columns, df=df_market_mapping):
 
         """Get the n largest value for a given column on df_market_mapping"""
 
-        top_variations = df_market_mapping[["country",f"{columns}"]].nlargest(n=number, columns=columns)
+        top_variations = df_market_mapping[["country", f"{columns}"]].nlargest(
+            n=number, columns=columns
+        )
         top_variations = top_variations.drop_duplicates(subset="country")
         top_variations.set_index("country", inplace=True)
 
@@ -99,7 +107,7 @@ class DataReader :
 
     def summary_metrics(self, df, currency_code):
 
-        df = df[df['currency_code'] == currency_code ]
+        df = df[df["currency_code"] == currency_code]
 
         average_pct = df["pct"].mean()
         max_pct = df["pct"].max()
@@ -117,27 +125,35 @@ class DataReader :
 
         ### Complementary metrics:
         self.average, self.max, self.min = self.summary_metrics(df, currency_code)
-        
+
         if self.average > 0:
-            return (f" The {currency_code} has been devalued in \
-                            average by {round(self.average,2)}% in the last {delta} days"), "devaluation"
+            return (
+                (
+                    f" The {currency_code} has been devalued in \
+                            average by {round(self.average,2)}% in the last {delta} days"
+                ),
+                "devaluation",
+            )
         else:
-            return (f"{currency_code} has been appreciated by\
-                {abs(round(self.average,2))}% relative to the USD in the last {delta} days"), "appreciation"
+            return (
+                (
+                    f"{currency_code} has been appreciated by\
+                {abs(round(self.average,2))}% relative to the USD in the last {delta} days"
+                ),
+                "appreciation",
+            )
 
+    def big_mac_exchange_rate(self, country_selected, df=df_big_mac):
 
-
-    def big_mac_exchange_rate(self, country_selected, df = df_big_mac):
-        
         """Retrieve last exchange for dollar big mac and date of last datapoint"""
         try:
-            df_big_mac_by_country = df[df['country'] == f'{country_selected}']
-            
+            df_big_mac_by_country = df[df["country"] == f"{country_selected}"]
+
             exchange = df_big_mac_by_country.dollar_ex.values[-1]
             date = df_big_mac_by_country.date.values[-1]
-            date = np.datetime_as_string(date, unit = "D")
+            date = np.datetime_as_string(date, unit="D")
 
             return exchange, date
-        
+
         except:
             return None, None
--- utils/visuals.py
+++ utils/visuals.py
@@ -3,6 +3,7 @@ import plotly.express as px
 import plotly.graph_objects as go
 import streamlit as st
 
+
 class Visuals:
 
     "Class with the main functions to visualize metrics of currencie's exchange"
@@ -10,36 +11,36 @@ class Visuals:
     def __init__(self):
 
         "Init the class"
-        
-    
+
     def map_country(self, df, country):
-    # Mapping country with currency code
+        # Mapping country with currency code
         if country == "Venezuela":
             # Returning values of new currency
             return "VED"
         else:
             return df[df.country == country].currency_code.values[0]
 
-
     ## Country selection:
     def plot_big_mac(self, df, values_y, country_selected):
 
         """Plot for Bic Mac Index price vs time
 
-            values: 'dollar_price' or 'dollar_ex'"""
+        values: 'dollar_price' or 'dollar_ex'"""
 
-        #[docs plot](https://plotly.com/python/plotly-express/) 
+        # [docs plot](https://plotly.com/python/plotly-express/)
 
         if country_selected in df.values:
 
-            df_by_country = df[df['country'] == f'{country_selected}']
+            df_by_country = df[df["country"] == f"{country_selected}"]
             df = df_by_country
-            values = df[f'{values_y}']
+            values = df[f"{values_y}"]
 
             fig = go.Figure()
-            fig.add_trace(go.Scatter(x=df.date, y=values,
-                    mode='lines+markers',
-                    name=country_selected))
+            fig.add_trace(
+                go.Scatter(
+                    x=df.date, y=values, mode="lines+markers", name=country_selected
+                )
+            )
 
             return fig
 
@@ -47,18 +48,19 @@ class Visuals:
 
             return "Country not available in Big Mac Data Base"
 
-    def add_trace_big_mac(self, df, values_y,country_selected, fig):
+    def add_trace_big_mac(self, df, values_y, country_selected, fig):
 
         if country_selected in df.values:
 
-            df_by_country = df[df['country'] == f'{country_selected}']
+            df_by_country = df[df["country"] == f"{country_selected}"]
             df = df_by_country
-            values = df[f'{values_y}']
+            values = df[f"{values_y}"]
 
-
-            fig.add_trace(go.Scatter(x=df.date, y=values,
-                    mode='lines+markers',
-                    name=country_selected))
+            fig.add_trace(
+                go.Scatter(
+                    x=df.date, y=values, mode="lines+markers", name=country_selected
+                )
+            )
 
             return fig
 
@@ -66,79 +68,80 @@ class Visuals:
 
             return None
 
-
     def plot_exchange(self, df, currency_code):
 
-        """ Calculates % variation of implicit exchange rate
+        """Calculates % variation of implicit exchange rate
         for a given fiat currency
         df: CurrenciesDataBase.db
         """
-        
-        df_plot = df[df['currency_code'] == currency_code ]
-        df_plot.sort_values(by = 'date', inplace = True)
-        fig = go.Figure()
-        fig.add_trace(go.Scatter(x=df_plot.date, y=df_plot.pct,
-                        mode='lines+markers',
-                        name=currency_code))
-        
-        return fig
 
+        df_plot = df[df["currency_code"] == currency_code]
+        df_plot.sort_values(by="date", inplace=True)
+        fig = go.Figure()
+        fig.add_trace(
+            go.Scatter(
+                x=df_plot.date, y=df_plot.pct, mode="lines+markers", name=currency_code
+            )
+        )
+
+        return fig
 
     def add_trace_exchange(self, df, currency_code, fig):
 
-        df_plot = df[df['currency_code'] == currency_code ]
-        df_plot.sort_values(by = 'date', inplace = True)
+        df_plot = df[df["currency_code"] == currency_code]
+        df_plot.sort_values(by="date", inplace=True)
 
-
-        fig.add_trace(go.Scatter(x=df_plot.date, y=df_plot.pct,
-                        mode='lines+markers',
-                        name=currency_code))
+        fig.add_trace(
+            go.Scatter(
+                x=df_plot.date, y=df_plot.pct, mode="lines+markers", name=currency_code
+            )
+        )
         return fig
-
 
     def update_layout(self, fig, title, x_axis, y_axis, legend):
         try:
             fig.update_layout(
-            title=title,
-            xaxis_title=x_axis,
-            yaxis_title=y_axis,
-            legend_title=legend)
+                title=title, xaxis_title=x_axis, yaxis_title=y_axis, legend_title=legend
+            )
         except:
             "fig object does not exist"
 
     @st.cache()
     def geo_scatter(self, df):
 
-        df = df[["country","volume_btc"]]
+        df = df[["country", "volume_btc"]]
 
-        fig = px.scatter_geo(df, locations="country", 
-                        locationmode= "country names", 
-                        size = "volume_btc",
-                        color= "volume_btc",
-                        projection="natural earth",
-                        #animation_frame= df['date'].dt.isocalendar().week,
-                        title= "Volume BTC")
+        fig = px.scatter_geo(
+            df,
+            locations="country",
+            locationmode="country names",
+            size="volume_btc",
+            color="volume_btc",
+            projection="natural earth",
+            # animation_frame= df['date'].dt.isocalendar().week,
+            title="Volume BTC",
+        )
 
         return fig
 
     @st.cache()
-    def world_map(self, df, average = False):
+    def world_map(self, df, average=False):
 
         if average == False:
             df = df.loc[df.pct > 0]
         else:
-            df = df.groupby(["country","currency_code"]).mean().reset_index()
+            df = df.groupby(["country", "currency_code"]).mean().reset_index()
             df = df.loc[df.pct > 0]
 
-        
-        fig = px.scatter_geo(df, locations="country", 
-                            locationmode= "country names", 
-                            size = "pct",
-                            color= "pct",
-                            projection="natural earth",
-                            title = "% Variation of foreign exchange rate"
-        #                      animation_frame= df['date'].dt.isocalendar().week
-                            )
+        fig = px.scatter_geo(
+            df,
+            locations="country",
+            locationmode="country names",
+            size="pct",
+            color="pct",
+            projection="natural earth",
+            title="% Variation of foreign exchange rate"
+            #                      animation_frame= df['date'].dt.isocalendar().week
+        )
 
         return fig
-
